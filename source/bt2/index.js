bdf0dee4a60688b06c0be48ce7d3b7f110b007bd
Hưng Trần <skymtp0503@gmail.com>
2021-12-21 13:44:10 +0700

    bai tap2

---
+++ source/bt2/index.js
@@ -0,0 +1,24 @@
+const listItem = document.querySelector('#selectInput');
+listItem.onchange = () => {
+  higthLight(listItem.value);
+};
+const higthLight = (value) => {
+  const listItem = document.getElementsByTagName('li');
+  for (i = 0; i < listItem.length; i++) {
+    if (value > 0 || value <= 6) {
+      if (value - 1 == i) listItem[i].style.background = 'yellow';
+      else listItem[i].style.background = 'none';
+    } else if (value == 'odd') {
+      if (i % 2 == 1) listItem[i].style.background = 'yellow';
+      else listItem[i].style.background = 'none';
+    } else if (value == 'even') {
+      if (i % 2 == 0) listItem[i].style.background = 'yellow';
+      else listItem[i].style.background = 'none';
+    } else if (value == 'even') {
+      if (i % 2 == 0) listItem[i].style.background = 'yellow';
+      else listItem[i].style.background = 'none';
+    } else if (value == 'reset') {
+      listItem[i].style.background = 'none';
+    }
+  }
+};
