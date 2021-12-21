a6e05c92cdcdd6c2888b2f1020030445cef91068
Hưng Trần <skymtp0503@gmail.com>
2021-12-21 14:13:39 +0700

    upcode bai2

--- source/bt2/index.js
+++ source/bt2/index.js
@@ -4,24 +4,22 @@ listItem.onchange = () => {
 };
 const higthLight = (value) => {
   const listItem = document.getElementsByTagName('li');
-  if (value > 0 || value <= 6) {
-    for (i = 0; i < listItem.length; i++) {
+  for (i = 0; i < listItem.length; i++) {
+    if (value > 0 || value <= 6) {
       if (value - 1 == i) listItem[i].style.background = 'yellow';
       else listItem[i].style.background = 'none';
-    }
-  } else if (value == 'odd') {
-    for (i = 0; i < listItem.length; i++) {
+    } else if (value == 'odd') {
       if (i % 2 == 1) listItem[i].style.background = 'yellow';
       else listItem[i].style.background = 'none';
-    }
-  } else if (value == 'even') {
-    for (i = 0; i < listItem.length; i++) {
+    } else if (value == 'even') {
       if (i % 2 == 0) listItem[i].style.background = 'yellow';
       else listItem[i].style.background = 'none';
-    }
-  } else if (value == 'reset') {
-    for (i = 0; i < listItem.length; i++) {
+    } else if (value == 'even') {
+      if (i % 2 == 0) listItem[i].style.background = 'yellow';
+      else listItem[i].style.background = 'none';
+    } else if (value == 'reset') {
       listItem[i].style.background = 'none';
     }
-  } else return;
+  }
 };
+
